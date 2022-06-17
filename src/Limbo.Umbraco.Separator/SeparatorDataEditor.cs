985820ec4f414ac20d06ed3303dc9a7efa04b834
Anders Bjerner <abjerner@skybrud.dk>
2022-06-17 22:40:56 +0200

    Bah

--- src/Limbo.Umbraco.Separator/SeparatorDataEditor.cs
+++ src/Limbo.Umbraco.Separator/SeparatorDataEditor.cs
@@ -10,7 +10,7 @@ namespace Limbo.Umbraco.Separator {
     [DataEditor(EditorAlias, EditorType.PropertyValue, "Limbo Separator", EditorView, Group = "Limbo", Icon = "icon-navigation-horizontal color-limbo", HideLabel = true)]
     public class SeparatorDataEditor : DataEditor {
 
-        internal const string EditorAlias = "Limbo.Separator";
+        internal const string EditorAlias = "Limbo.Umbraco.Separator";
 
         internal const string EditorView = "/App_plugins/Limbo.Umbraco.Separator/Views/Separator.html";
 
