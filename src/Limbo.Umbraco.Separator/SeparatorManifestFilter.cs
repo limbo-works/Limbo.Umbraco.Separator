9d67b5caef22a4f8a0aa0d0ccca077fd70223383
Anders Bjerner <abjerner@skybrud.dk>
2023-08-19 22:23:25 +0200

    Updated package manifest filter to set "PackageId" property in Umbraco 12

--- src/Limbo.Umbraco.Separator/SeparatorManifestFilter.cs
+++ src/Limbo.Umbraco.Separator/SeparatorManifestFilter.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Reflection;
 using Umbraco.Cms.Core.Manifest;
 
 namespace Limbo.Umbraco.Separator {
@@ -8,7 +9,9 @@ namespace Limbo.Umbraco.Separator {
 
         /// <inheritdoc />
         public void Filter(List<PackageManifest> manifests) {
-            manifests.Add(new PackageManifest {
+
+            // Initialize a new manifest filter for this package
+            PackageManifest manifest = new() {
                 AllowPackageTelemetry = true,
                 PackageName = SeparatorPackage.Name,
                 Version = SeparatorPackage.InformationalVersion,
@@ -16,7 +19,21 @@ namespace Limbo.Umbraco.Separator {
                 Stylesheets = new[] {
                     $"/App_Plugins/{SeparatorPackage.Alias}/Styles/Default.css"
                 }
-            });
+            };
+
+            // The "PackageId" property isn't available prior to Umbraco 12, and since the package is build against
+            // Umbraco 10, we need to use reflection for setting the property value for Umbraco 12+. Ideally this
+            // shouldn't fail, but we might at least add a try/catch to be sure
+            try {
+                PropertyInfo? property = manifest.GetType().GetProperty("PackageId");
+                property?.SetValue(manifest, SeparatorPackage.Alias);
+            } catch {
+                // We don't really care about the exception
+            }
+
+            // Append the manifest
+            manifests.Add(manifest);
+
         }
 
     }
